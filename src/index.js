4b56b562b54c8a2f361ac19824369b0a989945c5
Luiz Correa <luiz.correa@laborit.com.br>
2021-03-09 21:00:21 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,153 @@
+const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+const app = express()
+
+app.use(express.json())
+
+const customers = []
+
+// Middleware
+
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers
+
+    if (!cpf) {
+        return response.status(401).send({ error: 'You need provided a header cpf'})    
+    }
+
+    const customer = customers.find(customer => customer.cpf === cpf)
+
+    if (!customer) {
+        return response.status(400).send({ error: 'Customer not found'})
+    }
+
+    request.customer = customer
+
+    return next()
+}
+
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === 'credit') {
+            return acc + operation.amount
+        } else {
+            return acc - operation.amount
+        }
+    }, 0)
+
+    return balance
+}
+
+/**
+ * Create account
+ */
+app.post('/account', (request, response) => {
+    const { cpf, name } = request.body;
+
+    const customerAlreadyExists = customers.some(customer => customer.cpf === cpf)
+
+    if (customerAlreadyExists) {
+        return response.status(400).send({ error: 'Customer already exists'})
+    }
+
+    customers.push({
+        id: uuidv4(),
+        name,
+        cpf,
+        statement: []
+    })
+
+    return response.status(201).send()
+})
+
+app.use(verifyIfExistsAccountCPF)
+
+/**
+ * Get statement for user
+ */
+app.get('/statement', (request, response) => {
+    const { customer } = request
+    
+    return response.json(customer.statement)
+})
+
+app.get('/statement/date', (request, response) => {
+    const { customer } = request
+    const { date } = request.query
+
+    const dateFormat = new Date(date + " 00:00")
+
+    const statement = customer.statement.filter(statement => statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+    
+    return response.json(statement)
+})
+
+app.post("/deposit", (request, response) => {
+    const { customer } = request
+    const { description, amount } = request.body
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: 'credit'
+    }
+
+    customer.statement.push(statementOperation)
+
+    return response.status(201).send()
+})
+
+app.post("/withdraw", (request, response) => {
+    const { customer } = request
+    const { amount } = request.body
+
+    const balance = getBalance(customer.statement)
+
+    if (balance < amount) {
+        return response.status(400).send({ error: 'Insuficient funds!'})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: 'debit'
+    }
+
+    customer.statement.push(statementOperation)
+
+    return response.status(201).send()
+})
+
+app.put('/account', (request, response) => {
+    const { name } = request.body
+    const { customer } = request
+
+    customer.name  = name
+
+    return response.status(202).send()
+})
+
+app.get('/account', (request, response) => {
+    const { customer } = request
+
+    return response.json(customer)
+})
+
+app.delete('/account', (request, response) => {
+    const { customer } = request
+
+    customers.splice(customer, 1)
+
+    return response.status(204).send()
+})
+
+app.get("/balance", (request, response) => {
+    const { customer } = request
+
+    const balance = getBalance(customer.statement)
+
+    return response.json(balance)
+})
+
+app.listen(3333)
